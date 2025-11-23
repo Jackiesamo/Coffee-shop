d4a40651dbaf18c841606f2fea3e2e9540f7a8f7
jacklyne <jacklyne.owuor@student.moringaschool>
2025-11-23 22:48:00 +0300

    added some changes in get and settet method

--- coffee.py
+++ coffee.py
@@ -4,4 +4,15 @@ class Coffee:
         self.name =name
 
         @property
+        def name(self):
+          return self ._coffee
+     
+        @name.setter
+        def name(self, value):
+         if not isinstance(value, str):
+            print("Coffee name must be a string.")
+         if len(value) < 3:
+            raise TypeError("Coffee name must be at least 3 characters long")
+     
+        self ._coffee = value
 
