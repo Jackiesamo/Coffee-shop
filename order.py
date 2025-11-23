2518c47dc418a51e0b0151748f0490edf4ffd218
jacklyne <jacklyne.owuor@student.moringaschool>
2025-11-23 21:49:35 +0300

    added get snd setter method for customer property

--- order.py
+++ order.py
@@ -6,14 +6,51 @@ class Order:
      self.customer =customer
      self.coffee =coffee
      self.price =price
+
+     #add this order to the list of orders
      Order.orders.append(self)
 
      @property
      def customer(self):
-        return self.__customer
+        return self ._customer
      
      @customer .setter
-     def customer(self,customer):
+     def customer(self,value):
+        self ._customer = value
+        if not isinstance(value,customer):
+           print('customer must be an instance of customer class')
+        self ._customer = value
+
+        @property
+        def coffee(self):
+          return self ._coffee
+     
+     @coffee .setter
+     def coffee(self,value):
+        self ._coffee = value
+        if not isinstance(value,coffee):
+           print('coffee must be an instance of coffee class')
+        self ._coffee = value
+
+        @property
+        def price(self):
+          return self ._price
+     
+     @price .setter
+     def price(self,value):
+        self ._price = value
+        if not isinstance(value,float):
+           print('price must be a float')
+        if not(1.0<=value<=10.0):
+           raise TypeError('price must be between 1.0 and 10.0')
+        self ._customer = value
+
+
+
+
+
+
+           
 
 
     
